04dd259c58ca63318594283766e9f20ccce0c5ea
Vitaliy Delidov <Vitaliy@Vitaliys-Mac-Pro.local>
2014-11-27 16:42:53 +0200

    Initial Commit

---
+++ WebSocketTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebSocketTest
+//
+//  Created by Vitaliy Delidov on 11/27/14.
+//  Copyright (c) 2014 Vitaliy Delidov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebSocketTestTests/WebSocketTestTests.m
@@ -0,0 +1,40 @@
+//
+//  WebSocketTestTests.m
+//  WebSocketTestTests
+//
+//  Created by Vitaliy Delidov on 11/27/14.
+//  Copyright (c) 2014 Vitaliy Delidov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WebSocketTestTests : XCTestCase
+
+@end
+
+@implementation WebSocketTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
